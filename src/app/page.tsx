0cdbfd32937d7747eaa0b05da26852f0c9c3e0da
Kelvin Marcondes <kelvindesouza@hotmail.com>
2024-08-28 00:05:15 -0300

    Adicionado tema dark e white automatico

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Widget } from "@/components/Widget";
 
 export default function Home() {
   return (
-    <main className="p-16 min-h-screen bg-violet-950 items-center flex justify-center text-zinc-50 ">
+    <main className="p-16 min-h-screen bg-violet-400 dark:bg-violet-950 items-center flex justify-center text-zinc-50 ">
       <Widget />
     </main>
   );
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -23,29 +23,44 @@ export function Widget() {
 
     return(
         <div className="w-[448px] rounded overflow-hidden">
+            
             {/* Header */}
-            <div className="bg-zinc-800 py-4 px-6 flex items-center justify-between">
-                <span className="font-bold ">Notificações</span>
-                <button className="text-violet-500 text-xs hover:text-violet-400">
+            <div className="bg-zinc-200 dark:bg-zinc-800 py-4 px-6 flex items-center justify-between">
+                
+                <span className="font-bold text-stone-950 dark:text-zinc-100">
+                    Notificações
+                </span>
+                
+                <button className="font-semibold text-xs
+                                     text-violet-950 hover:text-violet-700
+                                     dark:text-violet-500 dark:hover:text-violet-400">
                     MARCAR TODAS COMO VISTAS
                 </button>
             </div>
 
             {/* Recent Section */}
-            <div className="bg-zinc-950 px-5 py-1 text-sm text-zinc-400">
+            <div className="px-5 py-1 text-sm 
+                            text-zinc-950 bg-zinc-400
+                            dark:text-zinc-400 dark:bg-zinc-950">
                 Recentes
             </div>
-            
-            <div className="divide-y-2 divide-zinc-950">
+            <div className="divide-y-2 divide-zinc-400
+                                       dark:divide-zinc-950">
                 {data.map(item => (
-                    <div key={item.id} className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+                    <div key={item.id} className="px-8 py-4 flex items-start gap-6
+                                                    bg-zinc-300
+                                                    dark:bg-zinc-900">
                         
                         <Rocket className="h-6 w-6 text-violet-500 mt-4"/>            
                         <div className="flex-1 flex flex-col gap-2">
-                            <p className="text-sm leading-relaxed text-zinc-100">
+                            <p className="text-sm leading-relaxed font-medium
+                                            text-zinc-950
+                                            dark:text-zinc-100">
                                 {item.text}
                             </p>
-                            <div className="text-xxs text-zinc-400 flex items-center gap-1">
+                            <div className="text-xxs flex items-center gap-1 
+                                            text-zinc-700 
+                                            dark:text-zinc-400">
                                 <span>Convite</span>
                                 <span>-</span>
                                 <span>{item.time}</span>
@@ -68,28 +83,40 @@ export function Widget() {
             </div>
 
             {/* Old Section */}
-            <div className="bg-zinc-950 px-5 py-1 text-sm text-zinc-400">
+            <div className="px-5 py-1 text-sm 
+                            text-zinc-950 bg-zinc-400
+                            dark:text-zinc-400 dark:bg-zinc-950">
                 Antigas
             </div>
 
-            <div className="divide-y-2 divide-zinc-950">
-                {data.map(item => (
-                    <div key={item.id} className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
+            <div className="divide-y-2 divide-zinc-400
+                                       dark:divide-zinc-950">
 
-                        <Rocket className="h-6 w-6 text-violet-500 mt-4"/>
+            {/* método apenas para repetir o data 2x e popular as noticações antigas */}
+            {data.flatMap(item => [item, item]).map(item => (
+                    <div key={item.id} className="px-8 py-4 flex items-start gap-6
+                                                    bg-zinc-300
+                                                    dark:bg-zinc-900">
+                        
+                        <Rocket className="h-6 w-6 text-violet-500 mt-4"/>            
                         <div className="flex-1 flex flex-col gap-2">
-                            <p className="text-sm leading-relaxed text-zinc-400">
+                            <p className="text-sm leading-relaxed 
+                                            text-zinc-900
+                                            dark:text-zinc-400">
                                 {item.text}
                             </p>
-                            <div className="text-xxs text-zinc-400 flex items-center gap-1">
+                            <div className="text-xxs flex items-center gap-1 
+                                            text-zinc-700 
+                                            dark:text-zinc-400">
                                 <span>Convite</span>
                                 <span>-</span>
                                 <span>{item.time}</span>
-                            </div>
+                            </div>                
                         </div>
-                    </div>
+                    </div>                
                 ))}
             </div>
+
         </div>      
     )
 }
